2c86f52fdd42165c1cfca5b3fd1ad67733fa5b72
M. Rummens <michel.rummens@hva.nl>
2017-12-01 18:25:44 +0100

    Upload New File

---
+++ Includes/dbconn.inc.php
@@ -0,0 +1,7 @@
+<?php
+$dbServername = "localhost:3306";
+$dbUsername = "root";
+$dbPassword = "TkPcMG109";
+$dbName = "contact";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
