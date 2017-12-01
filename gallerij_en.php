273907e82892a9ef0f74270f709ef49c8c75e451
M. Rummens <michel.rummens@hva.nl>
2017-12-01 18:30:14 +0100

    Upload New File

---
+++ gallerij_en.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="gallerij.css">
+<title>Photo booth</title>
+</head>
+<body>
+
+<img src="images/corendonTransparant.png" alt="Logo Corendon" style="width:262px;height:48px;">
+<br></br><br></br>
+<nav>
+	<ul>
+		<li><a href="index_en.php">Homepage</a></li>
+		<li><a class="active" href="gallerij_en.php">Gallery</a></li>
+		<li><a href="contact_en.php">Contact</a></li>
+                <li style="float:right"><a href="gallerij.php"><img src="images/Netherlands-Flag.png" alt="NL_vlag" class="t-image" style="width:30px;height:30px;border:0"></a></li>
+                <li style="float:right"><a href="gallerij_en.php"><img src="images/United-Kingdom-flag-icon.png" alt="UK_vlag" class="t-image" style="width:30px;height:30px;border:0"></a></li></ul>
+</nav>
+<br></br>
+<h1> The gallery </h1>
+<p> You can find pictures that are taken with the booth at different locations in the gallery. <br>
+With these pictueres we want to give you a vision of the possibilities of the booth.
+
+
+<div class="gallerij">
+	<a target="_blank" href="images/gallerij1.jpg">
+		<img src="images/gallerij1.jpg" alt="Gallerij1" width="300" height="200">
+	</a>
+	<div class="desc">This picture was captured in Antalya</div>
+</div>
+
+<div class="gallerij">
+	<a target="_blank" href="images/gallerij2.jpg">
+		<img src="images/gallerij2.jpg" alt="Gallerij2" width="300" height="200">
+	</a>
+	<div class="desc">This picture was captured in Madrid</div>
+</div>
+
+<div class="gallerij">
+	<a target="_blank" href="images/gallerij3.jpg">
+		<img src="images/gallerij3.jpg" alt="Gallerij3" width="300" height="200">
+	</a>
+	<div class="desc">This picture was captured in Paris</div>
+</div>
+
+        <div class="footer">
+            <p><a href=http://www.corendon.nl/>www.corendon.nl</a> | Corendon Copyright© 2017 | <a href="tel:023-7510606">023-7510606</a></p>
+        </div>
+
+
+
+    </body>
+</html>
