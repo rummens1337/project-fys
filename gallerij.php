8fc32da9ad52fb4f49044ce474a81f893b7e1d27
M. Rummens <michel.rummens@hva.nl>
2017-12-01 18:29:53 +0100

    Upload New File

---
+++ gallerij.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="gallerij.css">
+        <title>Fotokiosk</title>
+    </head>
+    <body>
+
+        <img src="images/corendonTransparant.png" alt="Logo Corendon" style="width:262px;height:48px;">
+        <br></br><br></br>
+        <nav>
+            <ul>
+                <li><a href="index.php">Homepage</a></li>
+                <li><a class="active" href="gallerij.php">Gallerij</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li style="float:right"><a href="gallerij.php"><img src="images/Netherlands-Flag.png" alt="NL_vlag" class="t-image" style="width:30px;height:30px;border:0"></a></li>
+                <li style="float:right"><a href="gallerij_en.php"><img src="images/United-Kingdom-flag-icon.png" alt="UK_vlag" class="t-image" style="width:30px;height:30px;border:0"></a></li></ul>
+        </nav>
+        <br></br>
+        <h1> De gallerij </h1>
+        <p> In de gallerij zijn foto's te vinden die gemaakt zijn op verschillende locaties.<br>
+            Met deze foto's willen wij u een beeld geven wat de booth kan.<br>
+
+
+        <div class="gallerij">
+            <a target="_blank" href="images/gallerij1.jpg">
+                <img src="images/gallerij1.jpg" alt="Gallerij1" width="300" height="200">
+            </a>
+            <div class="desc">Deze foto is genomen in Antalya</div>
+        </div>
+
+        <div class="gallerij">
+            <a target="_blank" href="images/gallerij2.jpg">
+                <img src="images/gallerij2.jpg" alt="Gallerij2" width="300" height="200">
+            </a>
+            <div class="desc">Deze foto is genomen in Madrid</div>
+        </div>
+
+        <div class="gallerij">
+            <a target="_blank" href="images/gallerij3.jpg">
+                <img src="images/gallerij3.jpg" alt="Gallerij3" width="300" height="200">
+            </a>
+            <div class="desc">Deze foto is genomen in Parijs</div>
+        </div>
+
+        <div class="footer">
+            <p><a href=http://www.corendon.nl/>www.corendon.nl</a> | Corendon Copyright© 2017 | <a href="tel:023-7510606">023-7510606</a></p>
+        </div>
+
+
+
+    </body>
+</html>
