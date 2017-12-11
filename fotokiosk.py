94c8230aa02862ed77b54309fe47e22d92e67265
M. Rummens <michel.rummens@hva.nl>
2017-12-11 13:29:06 +0100

    OOP gemaakt door klassen toe te voegen, imports staan nu in importskiosk E-mail versturen toegevoegd

--- fotokiosk.py
+++ fotokiosk.py
@@ -1,22 +1,12 @@
 #importeert alle nodige libraries.
 #de python libraries staan opgeslagen in /usr/local/lib
-from gpiozero import Button
-from picamera import PiCamera
-from time import gmtime, strftime
-from tkinter import *
-from guizero import App, PushButton, Text, Picture
-from Devices import *
-from Phidget22.PhidgetException import *
-from Phidget22.Phidget import *
-from Phidget22 import *
-from Phidget22.Devices.DigitalInput import *
-import requests
+from importskiosk import *
 
 #maakt de foto, slaat deze op d.m.v. output.
 def take_picture():
     global output
     output = strftime("/home/pi/fotokiosk/image-%d-%m %H:%M.png", gmtime())    
-    print("take a picture")
+    print("taken a picture")
     camera.capture(output)
     camera.stop_preview()
     
@@ -35,26 +25,28 @@ def onAttachHandler(e):
     
 #waar de foto opgeslagen wordt, ook de huidige tijd wordt opgeslagen.    
 output = ""
+
 #maakt een nieuwe window met de resolutie 800 480
 app = App("De Fotokiosk", 800, 480)
 message = Text(app, "IT103    groep2")
 new_pic = PushButton(app, take_picture, text="Nederlands")
 new_pic = PushButton(app, take_picture, text="Engels")
+
 camera = PiCamera()
 camera.resolution = (800, 480)
 camera.hflip = True
 camera.start_preview(alpha=200)
 
-
 #Dit is de input voor de knop
-ch = DigitalInput()	
+ch = DigitalInput()
 ch.openWaitForAttachment(5000)
 
-ch = DigitalInput()	
+ch = DigitalInput()
 ch.setOnAttachHandler(onAttachHandler)
 ch.setOnStateChangeHandler(onStateChangeHandler)
 ch.open()
 #Tot hier
+
 app.display()
 
 url = 'http://rummens1337.nl/includes/upload.inc.php'
@@ -62,3 +54,8 @@ files = {'file': open(output,'rb')}
 values = {'submit': 1}
 r = requests.post(url, data=values, files=files)
 print (r.text)
+code = (r.text)
+#Roept klasse sendmail aan en geeft dit als input voor het versturen van de foto
+email = 'Gilbertrishi@gmail.com'
+subject = 'Fotokiosk - Amsterdam'
+sendmail(email,subject,output,code)
